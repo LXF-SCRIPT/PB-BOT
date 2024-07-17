e48ee137c89b733678a7d18ca757d402c804a432
LXF-SCRIPT <elia2006grassini@gmail.com>
2024-07-17 03:58:21 +0200

    prova
    aaa

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.login('MTA5OTY0NDI1MzE3ODY5MTU5Nw.GWz1Xi.78pwdOcrc-zXmlFStbi-So2C49xKIiOmSbM_Zk');
+
+client.once('ready', () => {
+    console.log('Bot ONLINE');
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === '!youtube') {
+        message.channel.send('Iscriviti al mio canale: https://www.youtube.com/@Cerottino');
+    }
+
+    if (message.content === '!embed') {
+        const embed = new EmbedBuilder()
+            .setTitle('Link Discord')
+            .setDescription(`https://discord.gg/TaRNkcPP5W`)
+            .setThumbnail('https://cdn.discordapp.com/attachments/1053046164414529677/1217576628931268689/logonuovopb.png?ex=66982f99&is=6696de19&hm=39684290e74e5cb6d1acc798c9f0966285748658fa185d0bf1c78190483155bd&');
+
+        message.channel.send({ embeds: [embed] });
+    }
+});
+
+
+
